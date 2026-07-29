049f96199622c92843fd782957b374d59eebf4e1
shalinim157 <shalinim96157@gmail.com>
2026-07-29 21:17:55 +0530

    netlify origin add

--- mini-project/main.py
+++ mini-project/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-       "https://mini-project.netlify.app"
+       "https://mini-student-project.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
